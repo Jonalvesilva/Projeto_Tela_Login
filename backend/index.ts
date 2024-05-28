2a24704e45b8ec65f5386da5432e934c8f2e2fd1
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-28 16:23:57 -0300

    feat: add body-parser to expressServer

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,7 @@ import { SignInController } from "./controllers/signin.controller";
 import Container from "typedi";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import bodyParser = require("body-parser");
 dotenv.config();
 
 useContainer(Container);
@@ -12,6 +13,9 @@ const app = createExpressServer({
   cors: true,
   controllers: [SignInController], // we specify controllers we want to use
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.DB_CONNECTION as string);
 
