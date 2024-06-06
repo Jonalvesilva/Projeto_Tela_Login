60d4253dbef4e86f54b89439bf4265c0d248ed5f
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-06-06 13:53:02 -0300

    feat: add credentials cookie

--- backend/controllers/signin.controller.ts
+++ backend/controllers/signin.controller.ts
@@ -1,13 +1,16 @@
 import {
   JsonController,
-  Get,
   Body,
+  Res,
   Post,
   UseBefore,
 } from "routing-controllers";
 import { SignInService } from "../services/signin.services";
 import { Service } from "typedi";
 import { encryptPasswordMiddleware } from "../middlewares/passwordEncrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 @Service()
 @JsonController("/")
@@ -15,15 +18,29 @@ export class SignInController {
   constructor(private readonly signinService: SignInService) {}
 
   @Post("signin")
-  async Login(@Body() body: any) {
+  async Login(@Body() body: any, @Res() res: any) {
     const response = await this.signinService.Login(body.email, body.senha);
+    const email = body.email;
+    if (response.success) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET as string, {
+        expiresIn: "1d",
+      });
+      res.cookie("token", token);
+    }
     return response;
   }
 
   @Post("signup")
   @UseBefore(encryptPasswordMiddleware)
-  async Add(@Body() body: any) {
+  async Add(@Body() body: any, @Res() res: any) {
     const response = await this.signinService.Add(body);
+    const email = body.email;
+    if (response.success) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET as string, {
+        expiresIn: "1d",
+      });
+      res.cookie("token", token);
+    }
     return response;
   }
 }
