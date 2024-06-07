7f24c5d09464064874792dabce04f55a5a7542b8
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-06-07 10:07:35 -0300

    feat: modify home.tsx

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+import AuthVerify from "@/components/AuthVerify";
+import { useRouter } from "next/navigation";
+import { destroyCookie } from "nookies";
+import { success } from "@/functions/toast";
+
 export default function Home() {
-  return <div>Home</div>;
+  const router = useRouter();
+  const logout = () => {
+    destroyCookie(null, "token", { path: "/" }); // Remove o cookie "token"
+    success("Logout realizado com sucesso!");
+    router.push("/"); // Redireciona para a página de login após logout
+  };
+  return (
+    <div className="p-5">
+      {" "}
+      <AuthVerify />
+      Home
+      <button
+        onClick={() => logout()}
+        className="p-2 bg-green-600 text-xl ml-5 rounded-xl text-white "
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
